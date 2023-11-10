6ad7eb53792f7cb8b99b9427c7bb2fb342e2f63a
Aaron Dizele Kafutshi <aaron@ngtech.ai>
2023-11-10 03:17:26 +0100

    feat: add vercel config

--- src/request.ts
+++ src/request.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const request = axios.create({
-    baseURL: "http://104.248.174.217:3900/",
+    baseURL: "/api/",
+    // baseURL: "http://104.248.174.217:3900/",
     headers: {
         Accept: 'application/json',
         'Content-Type': 'application/json',
